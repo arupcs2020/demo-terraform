f3b3d4efcfec789a98c9aba0ca18c1846f08f2d3
arupcs2020 <48618212+arupcs2020@users.noreply.github.com>
2019-03-28 08:47:40 +0530

    Create vars.tf

---
+++ modules/vpc/vars.tf
@@ -0,0 +1,10 @@
+####################################
+# VPC CIDR
+####################################
+variable "vpc_cidr" {}
+####################################
+variable "env_name" {}
+variable "domain_name" {}
+variable "key_name" {}
+variable "ami_stage" {}
+variable "region" {}
